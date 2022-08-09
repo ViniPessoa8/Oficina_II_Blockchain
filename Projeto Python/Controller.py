5f365f7e99166f775625b2ecc06cef23b37781e9
Vinícius Cavalcante de Brito Pessoa <vcbp.snf18@uea.edu.br>
2022-08-08 23:45:13 -0400

    fix: profile

--- Projeto Python/Controller.py
+++ Projeto Python/Controller.py
@@ -6,8 +6,13 @@ from Transaction import Transaction
 
 SESSION_DATA = 'data'
 SESSION_LOG = 'log'
-
 PATH_IMG_POKEBOLA = "res\pokebola.png"
+
+blockchain = Blockchain(2)
+sc = SmartContract(blockchain)
+
+if SESSION_DATA in st.session_state:
+    del st.session_state[SESSION_DATA]
 
 def initialize_session():
 
@@ -18,23 +23,16 @@ def initialize_session():
         st.session_state['data'] = {
             "Ash":      Treinador(1, ["Pikachu", "Bulbasaur"]),
             "Garry":    Treinador(2, ["Raichu", "Ivysaur"]),
-            "Lais":     Treinador(3, ["Rattata", "Bulbasaur"]),
-            "Vini":     Treinador(4, ["Charmander", "Bulbasaur"]),
+            "Lais":     Treinador(3, ["Rattata", "Bulbasaur", "Riolu", "Squirtle"]),
+            "Vini":     Treinador(4, ["Charmander", "Bulbasaur", "Mewtwo"]),
             "Veronica": Treinador(5, ["Squirtle", "Bulbasaur"]),
-            "Tersio":   Treinador(6, ["Eevee", "Bulbasaur"]),
-            "Eca":      Treinador(7, ["Riolu", "Bulbasaur"]),
-            "Poppi":    Treinador(8, ["Mewtwo", "Bulbasaur"])
+            "Tersio":   Treinador(6, ["Eevee", "Bulbasaur"])
         }
-    
-    pokedata = st.session_state[SESSION_DATA]
-    nomes_treinadores = pokedata.keys()
 
 def trocar(nome_treinador_01, nome_treinador_02, id_pokemon):
 
     global pokedata
 
-    blockchain = Blockchain(2)
-    sc = SmartContract(blockchain)
     transaction = Transaction(nome_treinador_01, nome_treinador_02, id_pokemon)
     sc.makeTransaction(transaction)
     sc.getData()
@@ -45,3 +43,5 @@ def trocar(nome_treinador_01, nome_treinador_02, id_pokemon):
     st.session_state[SESSION_LOG] = blockchain.toString()
 
     st.experimental_rerun()
+    
+initialize_session()
--- Projeto Python/pages/Profile.py
+++ Projeto Python/pages/Profile.py
@@ -2,7 +2,9 @@ import streamlit as st
 import Controller as control
 import requests
 
-data = st.session_state[control.SESSION_DATA]
+# control.reset_session()
+control.initialize_session()
+data = control.st.session_state[control.SESSION_DATA]
 
 def get_sprite_url(pokemon_name):
     api = f"https://pokeapi.glitch.me/v1/pokemon/{pokemon_name}"
@@ -20,6 +22,7 @@ def get_sprite_url(pokemon_name):
 
 def print_trainer(name = "Ash"):
     global data
+    data = st.session_state[control.SESSION_DATA]
     
     st.header(name)
     
@@ -30,9 +33,7 @@ def print_trainer(name = "Ash"):
     for i in range(len(pokemons)):
         with cols[i]:
             st.write(pokemons[i])
-            st.image(get_sprite_url(pokemons[i]),width=50)
-
-
+            st.image(get_sprite_url(pokemons[i]), width=50)
 
 for name in data.keys():
     print_trainer(name)
