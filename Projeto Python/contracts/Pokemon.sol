0bc258de82367f540a41a542ffd45c08722cf424
Vinícius Cavalcante de Brito Pessoa <vini.pessoa7@gmail.com>
2022-10-17 17:54:11 -0400

    feat: create pokemon contract

---
+++ Projeto Python/contracts/Pokemon.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.17;
+
+contract Pokemon {
+
+    string name;
+    uint8 life;
+    uint8 attack;
+    uint8 defense;
+
+    constructor (string memory _name, uint8 _attack, uint8 _defense) {
+        life = 100;
+        name = _name;
+        attack = _attack;
+        defense = _defense;
+    }
+
+    
+
+}
