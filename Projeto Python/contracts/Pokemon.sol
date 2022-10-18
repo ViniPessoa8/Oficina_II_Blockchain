0881456d9eddf0bc66446e1fc8944ec0d5790a70
Vinícius Cavalcante de Brito Pessoa <vini.pessoa7@gmail.com>
2022-10-17 20:11:10 -0400

    added 2 functions + modifier isOwner

--- Projeto Python/contracts/Pokemon.sol
+++ Projeto Python/contracts/Pokemon.sol
@@ -2,12 +2,23 @@ pragma solidity ^0.8.17;
 
 contract Pokemon {
 
+    // ATTRIBUTES
     string name;
     uint8 life;
     uint8 attack;
     uint8 defense;
     address owner;
 
+    // MODIFIERS
+    modifier isOwner{
+        require(owner == msg.sender);
+        _;
+    }
+
+    // EVENTS
+    event ownerChanged(address oldOwner, address newOwner);
+
+    // FUNCTIONS
     constructor (string memory _name, uint8 _attack, uint8 _defense) {
         life = 100;
         name = _name;
@@ -16,6 +27,13 @@ contract Pokemon {
         owner = msg.sender;
     }
 
-    
+    function changeOwner(address newOwner) public isOwner {
+        owner = newOwner;
+    }
+
+    function takeDamage(uint8 damage) public {
+        life -= damage;
+        if (life < 0) life = 0;
+    }
 
 }
